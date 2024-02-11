d59375222c28cb1cb9e2da8d889cbe2951a5a60f
Obaada <159489903+Obaada@users.noreply.github.com>
2024-02-11 02:10:41 +0000

    love calculator

--- main.py
+++ main.py
@@ -95,3 +95,35 @@ if extra_cheese == "Y":
   Bill += 1
 
 print(f"Your final bill is: ${Bill}.")
+#################################################################################################
+# love calculator
+print("The Love Calculator is calculating your score...")
+name1 = input() # What is your name?
+name2 = input() # What is their name?
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+name1 = name1.lower()
+name2 = name2.lower()
+TRUE = 0
+LOVE = 0
+answer = ""
+TRUE += name1.count("t") + name2.count("t")
+TRUE += name1.count("r") + name2.count("r")
+TRUE += name1.count("u") + name2.count("u") 
+TRUE += name1.count("e") + name2.count("e") 
+LOVE += name1.count("l") + name2.count("l") 
+LOVE += name1.count("o") + name2.count("o") 
+LOVE += name1.count("v") + name2.count("v") 
+LOVE += name1.count("e") + name2.count("e") 
+TRUE = str(TRUE)
+LOVE = str(LOVE)
+score = TRUE + LOVE
+score = int(score)
+
+if score < 10 or score > 90:
+  answer += ", you go together like coke and mentos."
+elif score > 40 and score < 50:
+  answer += ", you are alright together."
+else: 
+  answer += "."
+print(f"Your score is {score}" + answer)
